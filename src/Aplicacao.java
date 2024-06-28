7633ec37d3e8fd548b516276fe59c1fc127eed62
Dimas Junior <dimas.junior1989@hotmail.com>
2024-06-28 15:31:26 -0300

    meu primeiro commit

---
+++ src/Aplicacao.java
@@ -0,0 +1,15 @@
+import Aplicativo.Iphone;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Iphone LancamentoIphone = new Iphone();
+		
+		System.out.println(" testes");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Ferramentas/NavegadorNaInternet.java
@@ -0,0 +1,5 @@
+package Ferramentas;
+
+public interface NavegadorNaInternet {
+
+}
